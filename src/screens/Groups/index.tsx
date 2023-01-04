b08a0ddb28d9a2d1ba659b442c2136561e04c2e8
André Pereira Fonseca <andrepfonseca19@gmail.com>
2023-01-04 17:04:19 -0300

    Creating Group screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,42 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Container } from './styles';
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos']);
+
   return (
     <Container>
       <Header />
+      <Highlight
+        title="Turmas"
+        subtitle="Jogue com a sua turma"
+      />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            key={item}
+            title={item}/>
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1}}
+        ListEmptyComponent={() => (
+        <ListEmpty 
+          message="Que tal cadastrar a primeira turma?" 
+        />
+        )}
+      />
+
+      <Button
+        title="Criar nova turma"
+      />
+
     </Container>
   );
 }
