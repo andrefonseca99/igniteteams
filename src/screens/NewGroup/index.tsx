f1ca1be43574c11d51a6987da79188859e9e3984
André Pereira Fonseca <andrepfonseca19@gmail.com>
2023-01-09 17:26:17 -0300

    Handling exceptions

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import { Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Container, Content, Icon } from "./styles";
 import { groupCreate } from '@storage/group/groupCreate';
+import { AppError } from "@utils/AppError";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
@@ -14,10 +16,19 @@ export function NewGroup(){
 
     async function handleNew(){
         try{
+            if(group.trim().length === 0){
+                return Alert.alert("Novo Grupo", "Informe o nome da turma");
+            }
             await groupCreate(group);
             navigation.navigate('players', { group: group });
         }catch(error){
-          console.log(error);
+            if(error instanceof AppError){
+                Alert.alert("Novo Grupo", error.message);
+            }
+            else{
+                Alert.alert("Novo Grupo", "Não foi possível criar um novo grupo.");
+                console.log(error);
+            }
         }
     }
 
